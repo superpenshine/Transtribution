a4f4ab65acd9d8d421b5490c5990a94687b01657
superpenshine <superpenshine@gmail.com>
2020-02-19 01:14:07 -0800

    footer using flex-box, changed favicon.ico, changed title

--- startDev.bat
+++ startDev.bat
@@ -1,1 +1,2 @@
 start cmd /k "cd /d %~dp0\venv\Scripts && activate && cd /d %~dp0 && python manage.py runserver --setting aurora.settings.dev"
+start cmd /k "cd /d %~dp0\venv\Scripts && activate && cd /d %~dp0 && npm start"
